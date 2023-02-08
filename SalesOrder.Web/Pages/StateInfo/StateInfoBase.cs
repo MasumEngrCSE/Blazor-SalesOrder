6698391d264532372e9779c083c49a2954593c70
Md. Masum Billah <md.masum123@gmail.com>
2023-02-09 02:23:26 +0600

    update page state
    update page state

--- SalesOrder.Web/Pages/StateInfo/StateInfoBase.cs
+++ SalesOrder.Web/Pages/StateInfo/StateInfoBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using SalesOrder.Models.Dtos;
 using SalesOrder.Web.Services.Interface;
 
 namespace SalesOrder.Web.Pages.StateInfo
@@ -7,5 +8,13 @@ namespace SalesOrder.Web.Pages.StateInfo
     {
         [Inject]
         public IStateInfoService stateInfoService { get; set; }
+
+        public IEnumerable<StateInfoDto> stateInfos { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            stateInfos = await stateInfoService.GetStates();
+            //return base.OnInitializedAsync();
+        }
     }
 }
