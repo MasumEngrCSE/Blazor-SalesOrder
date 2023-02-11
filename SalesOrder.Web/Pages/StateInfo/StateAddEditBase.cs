e385a9cec2570cca21385b4bad250b2feafef586
Md. Masum Billah <md.masum123@gmail.com>
2023-02-12 02:05:35 +0600

    Create StateAddEditBase.cs

---
+++ SalesOrder.Web/Pages/StateInfo/StateAddEditBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using SalesOrder.Models.Dtos;
+using SalesOrder.Web.Services.Interface;
+
+namespace SalesOrder.Web.Pages.StateInfo
+{
+    public class StateAddEditBase : ComponentBase
+    {
+        [Inject]
+        public IStateInfoService stateInfoService { get; set; }
+
+        public IEnumerable<StateInfoDto> stateInfos { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            stateInfos = await stateInfoService.GetStates();
+            //return base.OnInitializedAsync();
+        }
+    }
+}
