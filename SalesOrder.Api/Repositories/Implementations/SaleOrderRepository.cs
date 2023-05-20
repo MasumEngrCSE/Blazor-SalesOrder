8691def402a15421452e20b8de477ff5dbf545a6
Md. Masum Billah <md.masum123@gmail.com>
2023-05-20 23:39:18 +0600

    add SaleOrderRepository

---
+++ SalesOrder.Api/Repositories/Implementations/SaleOrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using SalesOrder.Api.Repositories.Interfaces;
+using SalesOrder.Models.Dtos;
+
+namespace SalesOrder.Api.Repositories.Implementations
+{
+    public class SaleOrderRepository : ISaleOrderRepository
+    {
+        public async Task<SalesOrderDto> AddSalesOrder(SalesOrderDto salesOrder)
+        {
+            try
+            {
+
+                return salesOrder;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            //throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteSalesOrder(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SalesOrderDto> GetSalesOrder(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SalesOrderDto>> GetSalesOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SalesOrderDto> UpdateSalesOrder(SalesOrderDto salesOrder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SalesOrder.Api/Repositories/Interfaces/ISaleOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using SalesOrder.Models.Dtos;
+
+namespace SalesOrder.Api.Repositories.Interfaces
+{
+    public interface ISaleOrderRepository
+    {
+        Task<IEnumerable<SalesOrderDto>> GetSalesOrders();
+        Task<SalesOrderDto> GetSalesOrder(int Id);
+        Task<SalesOrderDto> AddSalesOrder(SalesOrderDto salesOrder);
+        Task<SalesOrderDto> UpdateSalesOrder(SalesOrderDto salesOrder);
+        Task<bool> DeleteSalesOrder(int Id);
+    }
+}
