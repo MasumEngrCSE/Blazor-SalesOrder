cab57a9698dcb9f8addbcf77361252d35199cb13
Md. Masum Billah <md.masum123@gmail.com>
2023-05-26 02:17:23 +0600

    Update SaleOrderRepository.cs

--- SalesOrder.Api/Repositories/Implementations/SaleOrderRepository.cs
+++ SalesOrder.Api/Repositories/Implementations/SaleOrderRepository.cs
@@ -181,9 +181,101 @@ namespace SalesOrder.Api.Repositories.Implementations
             }
         }
 
-        public Task<SalesOrderDto> UpdateSalesOrder(SalesOrderDto salesOrder)
+        public SalesOrderDto UpdateSalesOrder(SalesOrderDto salesOrder)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                #region OrderMaster
+                var obj = this._DBContext.orderMasters.FirstOrDefault(p=>p.Id==salesOrder.Id);
+                obj.CustomCode = salesOrder.CustomCode ?? "";
+                obj.OrderTitle = salesOrder.OrderTitle;
+                obj.OrderDate = salesOrder.OrderDate ?? DateTime.Now;
+                obj.StateId = salesOrder.StateId;
+                obj.Remarks = salesOrder.Remarks ?? "";
+                obj.UpdatedDate = DateTime.Now;
+                obj.UpdatedBy = salesOrder.UpdatedBy;
+
+                //this._DBContext.orderMasters.Add(obj);
+                this._DBContext.SaveChanges();
+                #endregion
+
+
+                #region OrderWindow
+                foreach (var item in salesOrder.SalesOrderWindowList)
+                {
+                    
+                   // var objSW = new OrderWindow();
+                    var objSW = this._DBContext.orderWindows.FirstOrDefault(p => p.Id == item.Id);
+                    if (objSW == null)
+                    {
+                        objSW = new OrderWindow();
+                        objSW.CreatedDate = DateTime.Now;
+                        objSW.CreatedBy = salesOrder.CreatedBy;
+
+                        this._DBContext.orderWindows.Add(objSW);
+                    }
+                    else
+                    {
+                        objSW.UpdatedDate = DateTime.Now;
+                        objSW.UpdatedBy = salesOrder.UpdatedBy;
+                    }
+                    objSW.OrderId = salesOrder.Id;
+                    objSW.WindowTitle = item.WindowTitle;
+                    objSW.Qty = item.WindowQty ?? 0;
+                    objSW.TotalSubElement = item.TotalSubElement ?? 0;
+
+                    this._DBContext.SaveChanges();
+
+                    item.Id = objSW.Id;
+
+                }
+
+                #endregion
+
+
+
+                #region MyRegion
+                foreach (var item in salesOrder.WindowSubElementList)
+                {
+                    //var objSE = new OrderWindowSubElement();
+                    var objSE = this._DBContext.orderWindowsSubElements.FirstOrDefault(p => p.Id == item.Id);
+                    if (objSE == null)
+                    {
+                        objSE = new OrderWindowSubElement();
+                        objSE.OrderWindowId = salesOrder.SalesOrderWindowList.FirstOrDefault(p => p.OrderWindowId == item.OrderWindowId).Id ?? 0;
+                        objSE.CreatedDate = DateTime.Now;
+                        objSE.CreatedBy = salesOrder.CreatedBy;
+
+                        this._DBContext.orderWindowsSubElements.Add(objSE);
+                    }
+                    else
+                    {
+                        objSE.UpdatedDate = DateTime.Now;
+                        objSE.UpdatedBy = salesOrder.UpdatedBy;
+                    }
+                   
+                    objSE.SubElement = item.SubElement ?? 0;
+                    objSE.SubElementType = item.SubElementType;
+                    objSE.Width = item.SubElementWidth ?? 0;
+                    objSE.Height = item.SubElementHeight ?? 0;
+
+
+                    this._DBContext.SaveChanges();
+                    item.Id = objSE.Id;
+                }
+
+                #endregion
+
+
+
+                return salesOrder;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
