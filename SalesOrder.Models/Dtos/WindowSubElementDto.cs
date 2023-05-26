c201b3cf7a4c7387360ebafedc9498062178558e
Md. Masum Billah <md.masum123@gmail.com>
2023-05-26 22:53:00 +0600

    Update WindowSubElementDto.cs

--- SalesOrder.Models/Dtos/WindowSubElementDto.cs
+++ SalesOrder.Models/Dtos/WindowSubElementDto.cs
@@ -11,6 +11,7 @@ namespace SalesOrder.Models.Dtos
         public int? Id { get; set; }
         public int? OrderId { get; set; }
         public int? OrderWindowId { get; set; }
+        public string? WindowTitle { get; set; }
         public int? SubElement { get; set; }
         public string SubElementType { get; set; }
         public int? SubElementWidth { get; set; }
