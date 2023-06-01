5607b6c02e0d3915c4456b8f87675058108e98b0
Md. Masum Billah <md.masum123@gmail.com>
2023-06-02 00:01:20 +0600

    Blazor Bootstrap v1.8.1 add

--- SalesOrder.Web/Program.cs
+++ SalesOrder.Web/Program.cs
@@ -1,3 +1,4 @@
+using BlazorBootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SalesOrder.Web;
@@ -9,7 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7147/") });
-
+builder.Services.AddBlazorBootstrap();
 //https://localhost:7147/
 //builder.HostEnvironment.BaseAddress
 
