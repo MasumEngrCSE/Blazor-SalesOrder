25f35b08738d7a9ad93f3482492b51a3accd318c
Md. Masum Billah <md.masum123@gmail.com>
2023-06-09 01:36:32 +0600

    update

---
+++ SalesOrder.Web/Services/Implementation/DashboardService.cs
@@ -0,0 +1,32 @@
+﻿using SalesOrder.Models.Dtos;
+using SalesOrder.Web.Services.Interface;
+using System.Net.Http.Json;
+
+namespace SalesOrder.Web.Services.Implementation
+{
+    public class DashboardService : IDashboardService
+    {
+
+        private readonly HttpClient httpClient;
+        public DashboardService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+
+        public async Task<SOBarChartDataset> GetSOMonthlyBarChart()
+        {
+            try
+            {
+                var barChartdata = await this.httpClient.GetFromJsonAsync<SOBarChartDataset>($"api/Dashboard/getSOMonthlyBarChart");
+
+                return barChartdata;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SalesOrder.Web/Services/Interface/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using SalesOrder.Models.Dtos;
+
+namespace SalesOrder.Web.Services.Interface
+{
+    public interface IDashboardService
+    {
+        Task<SOBarChartDataset> GetSOMonthlyBarChart();
+    }
+}
