3e5a674de71c3bdc4dcf2f0001a7032b235965f6
Md. Masum Billah <md.masum123@gmail.com>
2023-06-09 23:20:21 +0600

    update

--- SalesOrder.Api/Repositories/Implementations/DashboardRepository.cs
+++ SalesOrder.Api/Repositories/Implementations/DashboardRepository.cs
@@ -1,7 +1,10 @@
 ﻿using BlazorBootstrap;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SalesOrder.Api.Data;
 using SalesOrder.Api.Repositories.Interfaces;
 using SalesOrder.Models.Dtos;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SalesOrder.Api.Repositories.Implementations
 {
@@ -18,11 +21,49 @@ namespace SalesOrder.Api.Repositories.Implementations
         {
             try
             {
+
+                DateTime fdate = DateTime.Now.AddMonths(-12);
+                var today = DateTime.Now;
+
                 SOBarChartDataset barChartdata = new SOBarChartDataset();
 
-                barChartdata.Label = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun" };
+                var data = await ((from p in this._DBContext.orderMasters
+                                   join s in this._DBContext.stateInfo on p.StateId equals s.Id
 
-                barChartdata.Data = new List<double> { 50, 60, 30, 70, 40, 70 };
+                                   where p.OrderDate >= fdate && p.OrderDate <= today
+                                   select new SalesOrderDto
+                                   {
+                                       Id = p.Id,
+                                       OrderTitle = p.OrderTitle,
+                                       OrderDate = p.OrderDate,
+                                       StateId = p.StateId,
+                                       StateName = s.Name,
+                                   }
+                                  ).GroupBy(p => new { p.StateId,p.StateName })
+                                  .Select(group => new
+                                  {
+                                      StateId = group.Key.StateId,
+                                      StateName= group.Key.StateName,
+                                      TotalCount = group.Count()
+                                  }
+                                  )
+
+                                  ).ToListAsync();
+
+
+                List<string> lavelLst=new List<string>();
+                List<double> dataLst = new List<double>();
+                foreach (var itm in data)
+                {
+                    lavelLst.Add(itm.StateName);
+                    dataLst.Add(itm.TotalCount);
+                }
+                barChartdata.Label= lavelLst;
+                barChartdata.Data = dataLst;
+
+                //barChartdata.Label = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun" };
+
+                //barChartdata.Data = new List<double> { 50, 60, 30, 70, 40, 70 };
 
 
                 return barChartdata;
