8282afcc62b9c6637765a12529e841585d464d5f
Md. Masum Billah <md.masum123@gmail.com>
2024-02-17 01:01:35 +0600

    update
    update

---
+++ SO.API/Models/SampleDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SO.API.Models
+{
+    public class SampleDBContext : DbContext
+    {
+        public SampleDBContext(DbContextOptions<SampleDBContext> options): base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+}
